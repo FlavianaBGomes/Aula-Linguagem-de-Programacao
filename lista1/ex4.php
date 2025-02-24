f823dc892326d84638fc22d6d42b98c302e28e13
Flaviana Gomes <147269030+FlavianaBGomes@users.noreply.github.com>
2025-02-24 12:12:41 -0300

    Alterações

--- lista1/ex4.php
+++ lista1/ex4.php
@@ -10,21 +10,19 @@
     <h1>
     <h1>Exercício 4</h1>
         <form method="post" action="ex4resposta.php">
-                        
                         <div class="mb-3">
-                            <label for="valor1" class="form-label">valor 1</label>
+                            <label for="valor1" class="form-label">Valor 1</label>
                             <input type="text" id="valor1" name="valor1" class="form-control">
                         </div>
                     
                         <div class="mb-3">
-                            <label for="valor2" class="form-label">valor 2</label>
+                            <label for="valor2" class="form-label">Valor 2</label>
                             <input type="text" id="valor2" name="valor2" class="form-control">
                         </div>
                     
                     <button type="submit" class="btn btn-primary">Enviar</button>
                 </form>
 
-    </h1>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
 </html>
