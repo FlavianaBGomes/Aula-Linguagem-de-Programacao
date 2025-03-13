b30511d76b72a4d5d125f50de81ebce4b1a644a3
FlavianaBGomes <147269030+FlavianaBGomes@users.noreply.github.com>
2025-03-13 20:12:23 -0300

    array

--- Exemplos_em_aula/exemplo_array.php
+++ Exemplos_em_aula/exemplo_array.php
@@ -4,6 +4,14 @@
             $valores = $_POST["valor"]; //a variável post é também um array
             var_dump($valores);
 
+
+            $array1 = [0, 1, 2, 3];
+            $array2 = array("uva", "maçã", "banana");
+            $array3 = [
+                  "uva" => 3,
+                  "maçã" => 4,
+                  "banana" => 5
+            ];
       } catch (Exception $e) {
             echo "Erro: " . $e->getMessage();
       }
