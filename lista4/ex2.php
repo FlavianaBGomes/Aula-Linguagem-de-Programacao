9ea199ce35bf55ccc62b8f8ebe0979544618b69b
Flaviana Gomes <flavianabgomes@gmail.com>
2025-03-13 17:16:37 -0300

    exercicio2

--- lista4/ex2.php
+++ lista4/ex2.php
@@ -1,36 +1,24 @@
-<?php
-  declare(strict_types=1); //obriga tipagens para variáveis
-?>
 <!doctype html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Resposta</title>
+    <title>Formulario</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body class="d-flex flex-column justify-content-center align-items-center vh-100">
-    <h1>Reposta do Exercicio 01</h1>
-    <div class="border w-50 p-3">
+      
+      <h1>Exercicio 02</h1>
 
-      <?php 
-        function contarCaracteres(string $palavra): void {
-          echo "A palavra '$palavra' possui ". strlen($palavra). " caracteres";
-        }
+     <form method="post" action="exer02resposta.php" class="border w-50 p-3">  
+      <div class="mb-3">
+          <label for="palavra" class="form-label">Digite a palavra: </label>
+          <input type="text" id="palavra" name="palavra" class="form-control" required="">
+      </div>
 
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-          try {
-            $palavra = $_POST['palavra'];
-            contarCaracteres($palavra);
-
-          } catch(Exception $e){
-              echo $e->getMessage();
-          }
-        } 
-        
-
-      ?>
-    </div>
+      <button type="submit" class="btn btn-primary">Enviar</button>
+    </form>
+            
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
 </html>
--- lista4/ex2resposta.php
+++ lista4/ex2resposta.php
@@ -1,5 +1,5 @@
 <?php
-  declare(strict_types=1); //obriga tipagens para variáveis
+  declare(strict_types=1); //obriga que as variáveis sejam tipadas
 ?>
 <!doctype html>
 <html lang="en">
@@ -10,18 +10,20 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body class="d-flex flex-column justify-content-center align-items-center vh-100">
-    <h1>Reposta do Exercicio 01</h1>
+    <h1>Reposta do Exercicio 02</h1>
     <div class="border w-50 p-3">
 
       <?php 
-        function contarCaracteres(string $palavra): void {
-          echo "A palavra '$palavra' possui ". strlen($palavra). " caracteres";
+        function toUpperAndLower(string $palavra): void {
+          echo "to lower: ". strtolower($palavra);
+          
+          echo "TO UPPER: ". strtoupper($palavra);
         }
 
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
           try {
             $palavra = $_POST['palavra'];
-            contarCaracteres($palavra);
+            toUpperAndLower($palavra);
 
           } catch(Exception $e){
               echo $e->getMessage();
