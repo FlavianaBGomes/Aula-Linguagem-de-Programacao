4e5238ffd044b419f3335cd251b902107abb0084
Flaviana Gomes <flavianabgomes@gmail.com>
2025-04-10 17:07:35 -0300

    Alterações

--- Projeto/imoveis.php
+++ Projeto/imoveis.php
@@ -1,8 +1,9 @@
+
 <?php 
     require_once("cabecalho.php");
     echo "<h2> Usuário ".$_SESSION['usuario']."</h2>";
 ?>
-    <h1>Categorias</h1>
+    <h1>Imoveis</h1>
 <?php 
     require_once("rodape.php") 
 ?>
--- Projeto/sair.php
+++ Projeto/sair.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!doctype html>
 <html lang="en">
   <head>
@@ -18,10 +17,3 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
 </html>
-=======
-<?php
-
-  session_start();
-  session_destroy();
-  header("location: index.php");
->>>>>>> 4b77c1846927daeed172ef7e45383489b9c3f255
